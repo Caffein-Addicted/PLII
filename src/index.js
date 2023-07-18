1b87d281451a2d12e5756a26dfc586e4b02d1546
seonik <ikik.pd@gmail.com>
2023-07-18 14:00:56 +0900

    Feat. firebase 추가, prettierrc 추가

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import {app, auth, db} from "./firebase";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+console.log(app);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
   <React.StrictMode>
     <App />
