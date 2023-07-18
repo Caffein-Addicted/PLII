92d84ed2b62a90e0fe4941a96f0ae52c0fcdc7c5
bongho <wqoo2190@gmail.com>
2023-07-18 17:02:25 +0900

    add mainpages

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-import {app, auth, db} from "./firebase";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { app, auth, db } from './firebase';
 
-console.log(app);
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
